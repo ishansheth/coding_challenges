97ba90b0885104227daf5924f29f13d92a630f34
ishan <ishansheth@gmail.com>
2017-11-30 00:01:31 +0100

    .ADD level order traversal BST

---
+++ level_order_traversal_tree.cpp
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/tree-level-order-traversal/problem
+void levelOrder(node * root) {
+    if(!root)
+        return;
+    std::queue<node*> myqueue;
+    myqueue.push(root);
+    while(!myqueue.empty())
+    {
+        node* current = myqueue.front();
+        std::cout<<current->data<<" ";
+        if(current->left != NULL)
+            myqueue.push(current->left);
+        if(current->right != NULL)
+            myqueue.push(current->right);
+        myqueue.pop();
+        
+    }  
+}
+
